e185590e0a39773c5fc7b41a298de7a310bf83fe
linxuerou0129 <2490669282@qq.com>
2020-07-08 22:50:56 +0800

    0708

---
+++ .history/src/router/index_20200708223840.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Edit',
+    name: 'Edit',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Edit.vue')
+  },
+  {
+    path: '/Published',
+    name: 'Published',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Published.vue')
+  },
+  {
+    path: '/MyCollect',
+    name: 'MyCollect',
+    component: () => import(/* webpackChunkName: "about" */ '../views/MyCollect.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
